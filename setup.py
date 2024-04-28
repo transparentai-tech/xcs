b788d22002b6fce2f3974605ac55d5cfa40e53e1
Tim Standen <timmstanden@gmail.com>
2024-04-28 13:34:00 -0700

    Update stuff

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     keywords='xcs accuracy classifier lcs reinforcement machine learning',
     packages=['xcs', 'xcs.algorithms'],
     install_requires=[
-        'vectorface @ git@github.com:transparentai-tech/VectorFace.git@main'
+        'vectorface @ git+ssh://git@github.com:transparentai-tech/VectorFace.git@main'
         ],
     test_suite="tests",
     tests_require=["numpy"],
